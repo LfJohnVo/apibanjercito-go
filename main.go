80e541c2c54e250d8ee12eeeb7ee43bad286c28f
johnvo <vojohn95@gmail.com>
2020-02-24 17:09:49 -0600

    ya se genera xml

--- main.go
+++ main.go
@@ -8,39 +8,41 @@ import (
 
 func main() {
 	fmt.Println("*******COMENZANDO APLICACIÓN*********")
-	/*fmt.Println("Seleccione una opción:")
-	fmt.Println("[1] 1ZVBP8EM7D5276339")
-	fmt.Println("[2] 3FADP4AJ7EM169194")
-	fmt.Println("[3] Concurrente" )
-	fmt.Println("[0] Salir")
-	var n int
-	num, err := fmt.Scanf("%v", &n)
-	fmt.Println(err)
-	fmt.Println("Tu numero es: ", num)*/
 
-	
-	consumo("3FADP4AJ7EM169194")
+	vins := [2]string{"3FADP4AJ7EM169194", "1ZVBP8EM7D5276339"}
+	tamaño := len(vins)
 
+	for f := 0; f < tamaño; f++ {
+	consumo(vins[f])
+	}
 
-	/*jsonData := map[string]string{"firstname": "Nic", "lastname": "Raboy"}
-	jsonValue, _ := json.Marshal(jsonData)
-	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
-	}*/
 	fmt.Println("Finalizando aplicación....")
 }
 
-func consumo(vin string){
+func consumo(vin string) {
 	/* Comienza consumo de api*/
-	response, err := http.Get("http://201.163.186.154:8080/BANJERCITO/wsvins/tests/v1/"+vin)
+	response, err := http.Get("http://201.163.186.154:8080/BANJERCITO/wsvins/tests/v1/" + vin)
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
+		//fmt.Println(string(data))
+		crearXml(data, vin)
 	}
+	//fmt.Println("Termina consumo")
+}
+
+func crearXml(data []byte, vin string) {
+	//xmlString := string(data)
+	fmt.Println("Resultado " +vin+ " : ", data)
+	filename := vin+".xml"
+	//file, _ := os.Create(filename)
+	//xmlWriter := io.Writer(file)
+	// Write `Hello, world!` to test.txt that can read/written by user and read by others
+	err := ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+
 }
